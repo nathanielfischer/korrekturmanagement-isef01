03d48d6174847e053e8e1b0a84115b66574b5589
Nathaniel Fischer <nathaniel.f@web.de>
2025-04-22 14:41:14 +0200

    Login gelöst, durch upgrade auf next-auth@beta

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -11,14 +11,14 @@ export async function authenticate(prevState, formData) {
     try {
         await signIn('credentials', formData);
     } catch (error) {
-
-        switch (error.type) {
-            case 'CredentialsSignin':
-                return 'Falsche Anmeldedaten.';
-            default:
-                return 'Etwas ist schiefgelaufen.';
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
         }
-
         throw error;
     }
 }
