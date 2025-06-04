e445ab5aa8dfcc54ff62d9f38bf73be262e159fc
Nathaniel Fischer <nathaniel.f@web.de>
2025-06-04 11:38:00 +0200

    Grauen Hintergrund auf mobilen Geräten korrigiert

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -4,20 +4,15 @@ import LogoutButton from "@/app/ui/logout-button.js";
 
 export default function Layout({ children }) {
     return (
-        <div>
+        <div className="min-h-screen flex flex-col">
             <header className="grid grid-cols-3">
                 <div className="flex items-center justify-end grow" ></div>
                 <IuLogo className="flex items-center justify-center grow" />
-                {/* <div className="flex items-center justify-end grow"><p>Logout</p></div> */}
                 <LogoutButton className="flex items-center justify-end grow" />
             </header>
-            {/* <main className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-                <div className="grow md:overflow-y-auto">{children}</div>
-            </main> */}
-            <main className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-light-grey rounded-md">
-                <div className="grow p-2 md:overflow-y-auto md:p-12">{children}</div>
+            <main className="flex-1 bg-light-grey rounded-md">
+                <div className="min-h-full p-2 md:p-12">{children}</div>
             </main>
         </div>
-
     );
 }
